325150858707c25e4cab4bbb25b85f2cede6a121
Administrator <18770914133@163.com>
2019-05-06 15:59:20 +0800

    update zuul

---
+++ example-springBoot/example-zuul-service/src/main/java/org/example/zuul/service/App.java
@@ -0,0 +1,13 @@
+package org.example.zuul.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
